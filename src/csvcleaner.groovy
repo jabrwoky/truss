65bbf60f8227bf3363ddcf3a3982e6a86ec0fec7
jabrwoky <delaney.ed@gmail.com>
2019-01-27 11:07:15 -0500

    date converter working

--- src/csvcleaner.groovy
+++ src/csvcleaner.groovy
@@ -3,18 +3,18 @@ import com.xlson.groovycsv.CsvParser
 @Grab('com.xlson.groovycsv:groovycsv:1.3')
 import com.xlson.groovycsv.CsvParser
 
-import java.text.ParseException
-import java.text.SimpleDateFormat
 import java.time.Duration
 import java.time.Instant
 import java.time.ZoneId
 import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 String filePath = "/home/ed/projects/truss/doc/sample-with-broken-utf8.csv"
 File file = new File(filePath)
 BufferedReader br = null
 br = new BufferedReader(new FileReader(file))
 def csvData = CsvParser.parseCsv(br)
+/*
 println "Timestamp\t" +
         "Address\t" +
         "ZIP\t" +
@@ -23,11 +23,13 @@ println "Timestamp\t" +
         "BarDuration\t" +
         "TotalDuration\t" +
         "Notes"
+*/
 def outputMap = [:]
 Instant instant = Instant.now()
 //println "NOW: " + instant.toString()
 
 csvData.each {
+    outputMap['Timestamp'] = timeStringToIso(it.Timestamp)
     outputMap['Address'] = it.Address
     outputMap['ZIP'] = it.ZIP.padLeft(5, '0')
     outputMap['FullName'] = it.FullName.toUpperCase()
@@ -35,35 +37,8 @@ csvData.each {
     //println "fooduration     :" + formatDuration(it.FooDuration)
     outputMap['BarDuration'] = convertDuration(it.BarDuration)
     outputMap['TotalDuration'] = sumDurations(it.FooDuration, it.BarDuration)
-    outputMap['Notes'] = it.Notes
+    //outputMap['Notes'] = checkUTF8(it.Notes)
 
-    outputMap['Timestamp'] = it.Timestamp
-    SimpleDateFormat inputFormat = new SimpleDateFormat("MM/dd/yy HH:mm:ss a")
-    ZonedDateTime zonedDateTime = null
-    try {
-        zonedDateTime = inputFormat.parse(it.Timestamp).toInstant()
-                .atZone(ZoneId.of("America/Los_Angeles"))
-    }
-    catch (ParseException e) {
-        println "Invalid date format ${it.Timestamp}"
-        //e.printStackTrace()
-    }
-    /*   println "it.Timestamp           :" + it.Timestamp
-       println "zonedDateTime          :" + zonedDateTime
-       println "zonedDateTime New York :" + zonedDateTime.withZoneSameLocal(ZoneId.of("America/New_York"))
-       DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME
-       println "dateTimeFormatted      :" + dateTimeFormatter.format(zonedDateTime.withZoneSameLocal(ZoneId.of("America/New_York")))
-   *//*
-       TimeZone tz = TimeZone.getTimeZone("America/Los_Angeles");
-       DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'")
-       df.setTimeZone(tz);
-       String nowAsISO = df.format(zonedDateTime);
-
-   println "Iso date :" + nowAsISO
-
-       SimpleDateFormat outputFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss a")
-       println outputFormat(zonedDateTime)
-   */
 
     /*outputMap['Timestamp'] = it.Timestamp
     outputMap['Address'] = it.Address
@@ -74,14 +49,15 @@ csvData.each {
     outputMap['TotalDuration'] = it.TotalDuration
     outputMap['Notes'] = it.Notes
     */
-    println "FooDuration" + outputMap.FooDuration
+    /*println "FooDuration" + outputMap.FooDuration
     println "BarDuration" + outputMap.BarDuration
     println "TotalDuration" + outputMap.TotalDuration
+*/
 }
 
 
 private String sumDurations(String foo, String bar) {
-    convertMillisecondsToHourString( convertToMilliseconds(foo) + convertToMilliseconds(bar))
+    convertMillisecondsToHourString(convertToMilliseconds(foo) + convertToMilliseconds(bar))
 
 }
 
@@ -90,7 +66,6 @@ private String convertDuration(String durationString) {
 }
 
 private int convertToMilliseconds(String durationString) {
-    //println "Durationstring : " + durationString
     def parts = durationString.split(":") as List
     Duration d = Duration.ZERO
     int hours = Integer.parseInt(parts[0])
@@ -100,24 +75,11 @@ private int convertToMilliseconds(String durationString) {
 
 
     String secondsString = parts[2]
-    //println "secondsString:  " + secondsString
     def secondsParts = secondsString.split("\\.")
-
     int seconds = Integer.parseInt(secondsParts[0])
     int milliseconds = Integer.parseInt(secondsParts[1])
-    //println "secondsParts:  " + secondsParts
     int secondsToMs = seconds * 1000;
-    /*printf( sprintf (" hours converted %09d\t%09d\n",[hours, hoursToMs]))
-    printf( sprintf (" minutes converted %09d\t%09d\n",[minutes, minutesToMs]))
-    printf( sprintf (" seconds converted %09d\t%09d\n",[seconds, secondsToMs]))
-    printf( sprintf (" milliseconds converted %09d\t%09d\n",[milliseconds, milliseconds]))
-*/
     int totalMilliseconds = hoursToMs + minutesToMs + secondsToMs + milliseconds
-    //println "totalMilliseconds  :" + totalMilliseconds
-    //float totalHours = totalMilliseconds / 3600000
-    /*println "totalMilliseconds: " +totalMilliseconds
-    println (sprintf("%.2f",totalMilliseconds/ 3600000 as float))
-    */
     totalMilliseconds
 }
 
@@ -125,3 +87,40 @@ private String convertMillisecondsToHourString(int milliseconds) {
     float totalHours = milliseconds / 3600000 as float
     sprintf("%.2f", totalHours)
 }
+
+private String timeStringToIso(String timeString) {
+    println "timeString             :" + timeString
+    ZonedDateTime zonedDateTime = null
+    // 4/1/11 11:00:00 AM
+    String inputPattern = "M/d/yy h:mm:ss a";
+    DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(inputPattern).withZone(ZoneId.of("America/Los_Angeles"))
+    zonedDateTime = ZonedDateTime.parse(timeString, dateTimeFormatter);
+    println "zonedDateTime          :" + zonedDateTime
+
+    ZonedDateTime centralEastern = zonedDateTime.withZoneSameInstant(ZoneId.of("America/New_York"));
+    println "centralEastern         :" + centralEastern
+
+
+    println "zonedDateTime ISO-8601 format: " + zonedDateTime.toOffsetDateTime().toString()
+    zonedDateTime.toOffsetDateTime().toString()
+}
+
+//checkUTF8()
+
+private void checkUTF8(String myString) {
+    System.out.println(myString);
+    byte[] myBytes = null;
+
+    try {
+        myBytes = myString.getBytes("UTF-8");
+    }
+    catch (UnsupportedEncodingException e) {
+        println "String is not utf8: " + myString
+    }
+
+    for (int i = 0; i < myBytes.length; i++) {
+        System.out.println(myBytes[i]);
+    }
+    myString
+}
+
