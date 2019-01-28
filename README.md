0c40ee4d1c039ef274190def643bcba5cfd1aaba
Ed Delaney <you@example.com>
2019-01-27 21:42:37 -0500

    add basic error checking

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
-# Project Title
+# csvcleaner
 
 This project contains my solution to the Truss exercise  
+ork zone  
+This is a standalone groovy script that reads csv formatted data from stdin and writes reformatted csv data to stdout  
+
+* The TimeStamp field on the input is assumed to be Pacific time, and the output is the ISO-8601 representation of that same time in America/New Y
+* Zip codes are formatted as 5 digits with leading zeros
+* Name columns are converted to upper case
+* FooDuration and BarDuration are output as floating point seconds
+* TotalDuration is ignored on input, and is the sum of FooDuration and BarDurtion on output
+* Duration and TimeStamp errors cause the record to be skipped
+* Notes column is currently passed through with no editing
+
+I took the liberty to treat the problem as a utility script, so testing different imput files is manual.  
+
 
 ## Getting Started
 
@@ -9,30 +22,36 @@ This project contains my solution to the Truss exercise
 ### Prerequisites
 
 You will need Java 8 and Groovy
-I use sdkman to install and manage versions. If you don't have sdkman, see https://sdkman.io/install to install  
+I use sdkman to install and manage versions.  
+If you don't have sdkman, see https://sdkman.io/install to install  
+Then:  
+```
 sdk install java 8u111  
 sdk install groovy 2.5.5  
-
-
-### Installing
-
-If you have issues with downloading grapes for commons-logging, clear your groovy grapes
-rm -rf  ~/.groovy/grapes/commons-logging/
-You may have to repeat this before all the dependencies get resolved
+```
+### Troubleshooting
+If you have issues with downloading grapes for commons-logging, clear your groovy grapes directory  
+`rm -rf  ~/.groovy/grapes/commons-logging/  `  
+You may have to repeat this before all the dependencies get resolved  
 You can get more debugging information about dependency resolution by turning up the logging
 
-export JAVA_OPTS="-Dgroovy.grape.report.downloads=true -Divy.message.logger.level=4"
+`export JAVA_OPTS="-Dgroovy.grape.report.downloads=true -Divy.message.logger.level=4"
+`
 
-```
-until finished
-```
 
 End with an example of getting some data out of the system or using it for a little demo
 
-## Running the tests
+## Running
+```
+cd truss/src
+groovy csvcleaner < <path to input file>  
+# for example:
+groovy csvcleaner.groovy < ../doc/sample.csv
+```
 
-Explain how to run the automated tests for this system
+Errors are directed to stderr, you can use standard bash redirecton of stdout and stderr  
 
+``groovy csvcleaner.groovy < ../doc/sample.csv 2>error.log``
 
 ## Authors
 
--- src/csvcleaner.groovy
+++ src/csvcleaner.groovy
@@ -1,36 +1,52 @@
 @Grab('com.opencsv:opencsv:4.0')
-import com.opencsv.CSVWriter
-@Grab('com.opencsv:opencsv:4.0')
-import com.opencsv.CSVWriter
 @Grab('com.xlson.groovycsv:groovycsv:1.3')
-import com.xlson.groovycsv.CsvParser
-@Grab('com.xlson.groovycsv:groovycsv:1.3')
+import com.opencsv.CSVWriter
 import com.xlson.groovycsv.CsvParser
 
-import java.time.Duration
+
 import java.time.ZoneId
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
+import java.nio.charset.CharsetDecoder
+import java.nio.charset.CharsetEncoder
+import java.nio.ByteBuffer
+import java.nio.charset.CharacterCodingException
+import java.time.format.DateTimeParseException
 
 // Read in the csv data
-BufferedReader br = new BufferedReader(new InputStreamReader(System.in))
-def csvData = CsvParser.parseCsv(br)
+BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))
+def Iterator csvData = CsvParser.parseCsv(bufferedReader)
 
 // transform the data
-def outputMap = [:]
-def outputSet = []
-csvData.eachWithIndex {it, i ->
-    outputMap['Timestamp'] = timeStringToIso(it.Timestamp)
-    outputMap['Address'] = it.Address
-    outputMap['ZIP'] = it.ZIP.padLeft(5, '0')
-    outputMap['FullName'] = it.FullName.toUpperCase()
-    outputMap['FooDuration'] = convertDuration(it.FooDuration)
-    outputMap['BarDuration'] = convertDuration(it.BarDuration)
-    outputMap['TotalDuration'] = sumDurations(it.FooDuration, it.BarDuration)
-    outputMap['Notes'] = it.Notes
 
-    outputSet.add(outputMap.collect { it.value } as String[])
+def outputSet = []  // this will hold the final processed dataset
 
+try {
+
+    csvData.findAll { it ->
+        def outputMap = [:]  // I like maps, it's easier to debug
+        outputMap['Timestamp'] = timeStringToIso(it.Timestamp)
+        if (!outputMap['Timestamp']) { // something is wrong with the input, skip this line
+            System.err.println "timeStamp can't be formatted, skipping..."
+            return true // go to next line
+        }
+        outputMap['Address'] = it.Address
+        outputMap['ZIP'] = it.ZIP.padLeft(5, '0')
+        outputMap['FullName'] = it.FullName.toUpperCase()
+        outputMap['FooDuration'] = convertDuration(it.FooDuration)
+        outputMap['BarDuration'] = convertDuration(it.BarDuration)
+        if (!outputMap['FooDuration'] || !outputMap['BarDuration']) {
+            System.err.println "duration can't be formatted, skipping..."
+            return true // go to next line
+        }
+        outputMap['TotalDuration'] = sumDurations(it.FooDuration, it.BarDuration)
+        outputMap['Notes'] = it.Notes
+        //checkUTF8( it.Notes )
+        outputSet.add(outputMap.collect { it.value } as String[])
+    }
+} catch (MissingPropertyException e) {
+    System.err.println "Fatal error processing input file:" + e.getMessage()
+    System.exit(1)
 }
 
 // write the output
@@ -55,23 +71,32 @@ private writeCsvFromMap(stuffToPrint) {
 }
 
 private String sumDurations(String foo, String bar) {
-    convertMillisecondsToHourString(convertToMilliseconds(foo) + convertToMilliseconds(bar))
-
+    try {
+        convertMillisecondsToSecondsString(convertToMilliseconds(foo) + convertToMilliseconds(bar))
+    } catch (java.lang.NumberFormatException e) {
+        return null
+    }
 }
 
 private String convertDuration(String durationString) {
-    convertMillisecondsToHourString(convertToMilliseconds(durationString))
+    try {
+        convertMillisecondsToSecondsString(convertToMilliseconds(durationString))
+    } catch (java.lang.NumberFormatException e) {
+        System.err.println "Cannot parse duration:" + durationString
+        return null
+    }
 }
 
 private int convertToMilliseconds(String durationString) {
     def parts = durationString.split(":") as List
-    Duration d = Duration.ZERO
+
     int hours = Integer.parseInt(parts[0])
     int minutes = Integer.parseInt(parts[1])
+
     int hoursToMs = hours * 3600000;
     int minutesToMs = minutes * 60000;
 
-
+    // parse seconds and milliseconds on '.' 999.999
     String secondsString = parts[2]
     def secondsParts = secondsString.split("\\.")
     int seconds = Integer.parseInt(secondsParts[0])
@@ -81,38 +106,29 @@ private int convertToMilliseconds(String durationString) {
     totalMilliseconds
 }
 
-private String convertMillisecondsToHourString(int milliseconds) {
-    float totalHours = milliseconds / 3600000 as float
-    sprintf("%.2f", totalHours)
+private String convertMillisecondsToSecondsString(int milliseconds) {
+    sprintf("%.4f", milliseconds / 1000 as float)
 }
 
 private String timeStringToIso(String timeString) {
+    if (!timeString) {
+        System.err.println "timeString cannot be null"
+        return null
+    }
     ZonedDateTime zonedDateTime = null
     // 4/1/11 11:00:00 AM
     String inputPattern = "M/d/yy h:mm:ss a";
     DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(inputPattern).withZone(ZoneId.of("America/Los_Angeles"))
-    zonedDateTime = ZonedDateTime.parse(timeString, dateTimeFormatter);
+    try {
+        zonedDateTime = ZonedDateTime.parse(timeString, dateTimeFormatter)
+    } catch (DateTimeParseException e) {
+        System.err.println "Cannot parse date: " + timeString
+        return null
+    }
+
     // So at that instant in LA, what time is it in NY?
     ZonedDateTime easternDateTime = zonedDateTime.withZoneSameInstant(ZoneId.of("America/New_York"));
     easternDateTime.toOffsetDateTime().toString()
 }
 
-//checkUTF8()
-
-private void checkUTF8(String myString) {
-    System.out.println(myString);
-    byte[] myBytes = null;
-
-    try {
-        myBytes = myString.getBytes("UTF-8");
-    }
-    catch (UnsupportedEncodingException e) {
-        println "String is not utf8: " + myString
-    }
-
-    for (int i = 0; i < myBytes.length; i++) {
-        System.out.println(myBytes[i]);
-    }
-    myString
-}
 
